2c0c1ae45ae63f186f24936e7a04f0d828b2be0e
Lasse Skaalum <lskaal19@student.aau.dk>
2020-11-12 20:09:01 +0100

    Renamed folder to api and renamed the main script to app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const bodyParser = require(`body-parser`);   // For parsing the request. Makes a
 require(`express-async-errors`);             // With this we don't have to pass errors like next(err) but can just throw them instead
 
 /* Internal modules */
-const handleRoutes         = require(`./server/routing`);
-const handleErrors         = require(`./server/error_handler`);
-const middleWare           = require(`./server/middleware`);
-const consoleLogToFile     = require(`./server/helpers/consol_log_file`);
-const database             = require(`./server/models/database/Database`);
+const handleRoutes     = require(`./server/routing`);
+const handleErrors     = require(`./server/error_handler`);
+const middleWare       = require(`./server/middleware`);
+const consoleLogToFile = require(`./server/helpers/consol_log_file`);
+const database         = require(`./server/models/database/Database`);
 
 /* Setup */
 global.conf = require(`./server_settings`); // Load settings into grobal variable (available across all scripts)
@@ -33,4 +33,4 @@ handleRoutes(express, app);
 handleErrors(express, app);
 app.use((req, res) => res.sendStatus(404)); // Handles non-existing URI-requests. Has to be the last line before app.listen.
 
-app.listen(global.conf.port, () => console.log(`skaalum-tech listening at http://localhost:${global.conf.port}`));
+app.listen(global.conf.port, () => console.log(`api listening at http://localhost:${global.conf.port}/api`));
